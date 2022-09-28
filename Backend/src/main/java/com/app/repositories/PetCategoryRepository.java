5b8838989b3ed56fc8aeb0a569c287d2896db7c2
Ashish2769 <109667024+Ashish2769@users.noreply.github.com>
2022-09-28 16:43:09 +0530

    Delete Backend directory

--- Backend/src/main/java/com/app/repositories/PetCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entities.PetCategory;
-
-public interface PetCategoryRepository extends JpaRepository<PetCategory, Long>{
-
-}
